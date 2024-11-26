52281300353ba5374df84e8d188e74c329d38b24
RYKELLE <thouret.ruelan@gmail.com>
2024-11-26 22:04:15 +0800

    Update converter.s

--- src/converter.s
+++ src/converter.s
@@ -72,6 +72,47 @@ invalid_system:
     bl print_string
     b select_system
 
+    invalid_operation:
+    adrp x0, invalid_operation_msg@PAGE
+    add x0, x0, invalid_operation_msg@PAGEOFF
+    bl print_string
+    b select_operation
+
+get_second_number:
+    // Display input message for second number
+    adrp x0, input_msg2@PAGE
+    add x0, x0, input_msg2@PAGEOFF
+    bl print_string
+
+    // Read second number
+    bl read_string
+    mov x20, x0             // Reuse x20 for second number
+    mov x21, x1             // Reuse x21 for length
+
+    // Validate second input
+    mov x0, x20             // Input string
+    mov x1, x21             // Length
+    mov x2, x19             // Base choice
+    bl validate_input
+    cmp x0, #0
+    b.eq invalid_second     // If invalid, try again
+
+    // Convert second input to decimal
+    mov x0, x20             // Input string
+    mov x1, x21             // Length
+    mov x2, x19             // Base choice
+    bl str_to_int
+    mov x24, x0             // Store second number in x24
+
+    // Perform operation based on choice in x23
+    cmp x23, #1
+    b.eq do_addition
+    cmp x23, #2
+    b.eq do_subtraction
+    cmp x23, #3
+    b.eq do_multiplication
+    b do_division
+
 
 exit_program:
     mov x0, #0              // Exit code 0
